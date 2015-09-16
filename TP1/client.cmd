017314c46a0a77b6c6cdf44f16e5d5863380594e
Alexandre Rose <alexandre.rose.0@gmail.com>
2015-09-15 22:27:26 -0400

    edit windows scripts

--- TP1/client.cmd
+++ TP1/client.cmd
@@ -1,3 +1,4 @@
+@echo off
 pushd .
 set serverbasepath=%CD%
 java -cp %serverbasepath%\bin -Djava.security.policy=%serverbasepath%\policy ca.polymtl.inf4402.tp1.client.Client
--- TP1/server.cmd
+++ TP1/server.cmd
@@ -1,3 +1,5 @@
+@echo off
+
 pushd .
 set serverbasepath=%CD%
 java -cp %serverbasepath%\bin -Djava.rmi.server.codebase=file:%serverbasepath%\shared.jar -Djava.security.policy=%serverbasepath%\policy ca.polymtl.inf4402.tp1.server.Server

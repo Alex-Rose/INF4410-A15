d25563ad74e8173f689b148e0b77729ca826d15b
Alexandre Rose <alros@l4712-03.info.polymtl.ca>
2015-11-12 20:29:38 -0500

    fix scripts for linux

--- TP2/src/ca/polymtl/inf4410/tp2/worker/Worker.java
+++ TP2/src/ca/polymtl/inf4410/tp2/worker/Worker.java
@@ -31,7 +31,7 @@ public class Worker implements ServerInterface {
             registry.rebind(config.getName(), stub);
             System.out.println("Server ready.");
         } catch (ConnectException e) {
-            System.err.println("Impossible de se connecter au registre RMI. Est-ce que rmiregistry est lancé ?");
+            System.err.println("Impossible de se connecter au registre RMI. Est-ce que rmiregistry est lance ?");
             System.err.println();
             System.err.println("Erreur: " + e.getMessage());
         } catch (Exception e) {

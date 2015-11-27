b3fd3d91cc2fb4bb6fb26f4651f6bc858e3df37d
Alexandre Rose <alros@l4712-07.info.polymtl.ca>
2015-11-26 21:55:44 -0500

    Question 1

--- TP3/fichiers_fournis/install.sh
+++ TP3/fichiers_fournis/install.sh
@@ -1,28 +1,33 @@
 #!/bin/bash
-
 # reponses au questions de mysql-server-5.5 et de phpmyadmin en utilisant les outils fournis dans le paquage debconf-utils
-
 # -----------------------------------
-
 # installation de mysql-server, mysql-client, apache2,  php5, libapache2-mod-php5, php5-mysql, phpmyadmin. Dans le meme ordre
 
-echo "Include /etc/phpmyadmin/apache.conf" | sudo tee --append /etc/apache2/apache2.conf
-sudo service apache2 restart
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
+DBROOT="root"
+DBPASS="your_password"
+MYPASS="your_password2"
 
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password your_password'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password your_password'
-sudo apt-get -y install mysql-server
-sudo service mysql restart
-sudo apt-get -y install apache2 php5 libapache2-mod-php5 php5-mysql 
+echo "Include /etc/phpmyadmin/apache.conf" | tee --append /etc/apache2/apache2.conf
+service apache2 restart
 
-sudo debconf-set-selections <<<  "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-sudo debconf-set-selections <<<  "phpmyadmin phpmyadmin/dbconfig-install boolean true" 
-sudo debconf-set-selections <<<  "phpmyadmin phpmyadmin/mysql/admin-user string root" 
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password your_password" 
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password your_password2" 
-sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password your_password2"
+debconf-set-selections <<< "mysql-server mysql-server/root_password password $DBPASS"
+debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $DBPASS"
+apt-get -y install mysql-server
+service mysql restart
+apt-get -y install apache2 php5 libapache2-mod-php5 php5-mysql mysql-client
 
-sudo apt-get -y install phpmyadmin
+debconf-set-selections <<<  "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
+debconf-set-selections <<<  "phpmyadmin phpmyadmin/dbconfig-install boolean true" 
+debconf-set-selections <<<  "phpmyadmin phpmyadmin/mysql/admin-user string $DBROOT" 
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $DBPASS" 
+debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $DBPASS" 
+debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $DBPASS"
 
-sudo service apache2 restart
+apt-get -y install phpmyadmin
+
+service apache2 restart
